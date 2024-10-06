626fe9a050b4cc56239c263797d9f44111f8707c
Your Name <veronichka.danilina@mail.ru>
2024-10-07 01:02:50 +0400

    fead: add solution rename.js and delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,12 @@
+import fs from 'node:fs';
+
 const remove = async () => {
-    // Write your code here 
+ const path = './src/fs/files/fileToRemove.txt';
+ const messageError = 'FS operation failed';
+
+ fs.rm(path, { recursive: true }, err => {
+  if (err) throw new Error(messageError);
+ });
 };
 
 await remove();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,19 @@
+import fs from 'node:fs';
+
 const rename = async () => {
-    // Write your code here 
+ const pathFile = './src/fs/files/wrongFilename.txt';
+ const pathRenameFile = './src/fs/files/properFilename.md';
+ const messageError = 'FS operation failed';
+
+ fs.access(pathRenameFile, fs.constants.F_OK, isPromiseResolve => {
+  if (isPromiseResolve) {
+   fs.rename(pathFile, pathRenameFile, err => {
+    if (err) throw new Error(messageError);
+   });
+  } else {
+   throw new Error(messageError);
+  }
+ });
 };
 
 await rename();
