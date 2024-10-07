0bdc3e212f7b3d56058f670b80769494f23107df
Your Name <veronichka.danilina@mail.ru>
2024-10-08 00:08:04 +0400

    fead: add solution calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import fs from 'node:fs';
+import crypto from 'node:crypto';
+
 const calculateHash = async () => {
-  
+ const path = './src/hash/files/fileToCalculateHashFor.txt';
+ const hash = crypto.createHash('sha256');
+ const stream = fs.createReadStream(path);
+ stream.on('data', (chunk) => {
+  hash.update(chunk);
+ });
+ stream.on('end', () => {
+  console.log(hash.digest('hex'));
+ });
 };
 
 await calculateHash();
