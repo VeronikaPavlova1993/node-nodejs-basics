7f012bff00990511b3d02b66a2b2ec36cbc1689e
Your Name <veronichka.danilina@mail.ru>
2024-10-07 11:38:52 +0400

    fead: add solution env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envVariables = 'RSS_';
+    const obj = process.env;
+    for (let prop in obj) {
+        if (prop.startsWith(envVariables)) {
+          console.log(`${prop}=${obj[prop]}`);
+        }
+      } 
 };
 
 parseEnv();
