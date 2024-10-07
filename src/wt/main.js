0d9c71bc3f7ef3531ab63afb53097566b45de195
Your Name <veronichka.danilina@mail.ru>
2024-10-07 22:50:35 +0400

    fead: add solution main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,28 @@
+import os from 'node:os';
+import { Worker } from 'node:worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+ const data = [...Array(os.availableParallelism()).keys()].map((elem) => {
+  return new Promise((resolve, reject) => {
+   const worker = new Worker('./src/wt/worker.js', {
+    workerData: elem + 10,
+   });
+   worker.on('message', (data) => {
+    resolve({
+     status: 'resolved',
+     data,
+    });
+   });
+   worker.on('error', () => {
+    reject({
+     status: 'error',
+     data: null,
+    });
+   });
+  });
+ });
+
+ console.log(await Promise.all(data));
 };
 
 await performCalculations();
