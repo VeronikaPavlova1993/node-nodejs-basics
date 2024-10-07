f69567f9d0ca66338dbe8050fe4374555fc026a3
Your Name <veronichka.danilina@mail.ru>
2024-10-07 10:16:15 +0400

    fead: add solution read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import fs from 'node:fs';
+
 const read = async () => {
-    // Write your code here 
+ const path = './src/fs/files/fileToRead.txt';
+ const messageError = 'FS operation failed';
+
+ fs.readFile(path, 'utf8', (err, data) => {
+  if (err) {
+   throw new Error(messageError);
+  } else {
+   console.log(data);
+  }
+ });
 };
 
 await read();
