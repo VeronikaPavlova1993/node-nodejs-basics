c4d325c69fe54e14d680ac47f8fb7085a2e24b9f
Your Name <veronichka.danilina@mail.ru>
2024-10-07 19:06:42 +0400

    fead: add solution transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import { Transform } from 'node:stream';
+import stream from 'stream/promises';
+
 const transform = async () => {
-    // Write your code here 
+ console.log('Please, enter the text');
+
+ const transformData = new Transform({
+  transform(chunk, encoding, callback) {
+   callback(null, `${[...chunk.toString()].reverse().join('')}\n`);
+  },
+ });
+
+ stream.pipeline(process.stdin, transformData, process.stdout);
 };
 
 await transform();
