ce147020b7723b88299250e48aa483943f86429e
Your Name <veronichka.danilina@mail.ru>
2024-10-07 14:32:33 +0400

    fead: add solution write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,11 @@
+import { createWriteStream } from 'node:fs';
+import stream from 'stream/promises';
+
 const write = async () => {
-    // Write your code here 
+ const writeStream = createWriteStream('./src/streams/files/fileToWrite.txt');
+ console.log('Enter the data to record to the file');
+
+ await stream.pipeline(process.stdin, writeStream);
 };
 
 await write();
